8c9a6d688fa315c6eedc2bc63a94c18ed2cbd9f0
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-05-12 04:57:47 -0500

    Update and rename install.sh to init/install.sh

--- init/install.sh
+++ init/install.sh
@@ -1,7 +1,13 @@
+name=xenial
+
 download_Crouton(){
   cd ~/Downloads
   wget -c https://github.com/dnschneid/crouton/raw/master/installer/crouton
   sudo install -Dt /usr/local/bin -m 755 ~/Downloads/crouton
+}
+
+brightness_and_volume(){
+  sudo crouton -r $name -t keyboard -u
 }
 
 install_Distro(){
