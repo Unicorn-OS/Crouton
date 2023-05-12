b74245a711657e14e92f7f1618806ebf153a7320
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-05-12 05:16:36 -0500

    Update and rename readme.md to remove.sh

---
+++ init/remove.sh
@@ -0,0 +1,8 @@
+uninstall_chroot(){
+  # https://www.reddit.com/r/Crouton/comments/kf80sc/how_do_i_uninstall_crouton_in_chrome_os/
+
+  sudo edit-chroot -l -a 
+  name=""
+
+  sudo delete-chroot "$name"
+}
--- remove/readme.md
+++
@@ -1,8 +0,0 @@
-https://www.reddit.com/r/Crouton/comments/kf80sc/how_do_i_uninstall_crouton_in_chrome_os/
-
-```
-sudo edit-chroot -l -a 
-name=""
-
-sudo delete-chroot "$name"
-```
