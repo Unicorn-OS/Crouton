49d7ba327b32dea8908a4079dd9c250b9ebf8a8e
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-05-12 05:22:58 -0500

    Create xiwi.sh

---
+++ init/target/xiwi.sh
@@ -0,0 +1,5 @@
+
+example(){
+  # https://medium.com/@rmcneely/setting-up-your-arm-chromebook-for-development-aa530d0cdb15
+  sudo sh ~/Downloads/crouton -r xenial -t chromium,x11,xiwi,xfce,audio,core,gtk-extra,xorg,cli-extra
+}
